52ab1d8f9c14233c447667114def902d413ac69f
Aleksi Peltola <aleksijaakkojuhani@gmail.com>
2022-10-09 21:52:11 +0300

    joo

--- app/src/routes/users.ts
+++ app/src/routes/users.ts
@@ -4,7 +4,7 @@ import { faker } from '@faker-js/faker';
 const router = express.Router();
 
 router.get('/api/users', async (req: Request, res: Response) => {
-  const names = Array.from({ length: 10 }).map((i) => faker.name.fullName());
+  const names = Array.from({ length: 10 }).map(() => faker.name.fullName());
 
   res.json(names);
 });
