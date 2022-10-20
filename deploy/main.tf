a148d4c864f4e37f21648f952fa80290db7d8a61
Aleksi Peltola <aleksijaakkojuhani@gmail.com>
2022-10-20 16:33:41 +0300

    terraform init

---
+++ deploy/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  backend "s3" {
+    bucket = "node-api-tfsate-allu"
+    key = "node-api.tfstate"
+    region = "eu-north-1"
+    encrypt = true
+    dynamodb_table = "node-api-ts-state-lock" # the table name has a typo, should be '...tf-state-lock' not 'ts' but oh well
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
